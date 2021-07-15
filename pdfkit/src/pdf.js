a58db4d82cc5f26b94ed5d0222b73dd2fa25029f
Your Name <you@example.com>
2021-07-15 10:48:16 +0800

    split pdf with 1000

--- pdfkit/src/pdf.js
+++ pdfkit/src/pdf.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 module.exports = function generate({ images, outputDir, cuttingImg, config } = { images: [] }) {
   const {
+    size = 1000,
     width: _width, height: _height,
     pageW: _pageW, pageH: _pageH,
     paddingX = 0, paddingY = 0,
@@ -15,62 +16,70 @@ module.exports = function generate({ images, outputDir, cuttingImg, config } = {
   const pageW = _pageW * ratio, pageH = _pageH * ratio
   const marginX = mx * ratio, marginY = my * ratio
   const pageNumX = _pageNumX * ratio, pageNumY = _pageNumY * ratio
-  // Create a document
-  // The size property can be either an array specifying [width, height] in PDF points (72 per inch), 
-  // or a string specifying a predefined size. 
-  const doc = new PDFDocument({
-    size: [pageW, pageH],//https://pdfkit.org/docs/paper_sizes.html
-    margins: {
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0
-    }
-  });
-  doc.pipe(fs.createWriteStream(path.join(outputDir, 'output.pdf')));
 
-  // Embed a font, set the font size, and render some text
-  // doc
-  //   .font('fonts/PalatinoBold.ttf')
-  //   .fontSize(25)
-  //   .text('Some text with an embedded font!', 100, 100);
-
-  // Add an image, constrain it to a given size, and center it vertically and horizontally
-
-  images.forEach((img, i) => {
-
-    const page = i % pageSize
-
-    if (page === 0 && i > 0) {
-      console.log('new pdf page', i)
-      // Add another page
-      doc.addPage()
-    }
-
-    const row = Math.floor(page / cols)
-    const col = page % cols
-    // console.log(row, col, img)
-    var _ = doc.image(img, col * (width + marginX) + paddingX, row * (height + marginY) + paddingY, {
-      width, height,
-      // fit: [150, 200],
-      // align: 'center',
-      // valign: 'center' 
+  function _func(images) {
+    // Create a document
+    // The size property can be either an array specifying [width, height] in PDF points (72 per inch), 
+    // or a string specifying a predefined size. 
+    const doc = new PDFDocument({
+      size: [pageW, pageH],//https://pdfkit.org/docs/paper_sizes.html
+      margins: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+      }
     });
+    doc.pipe(fs.createWriteStream(path.join(outputDir, 'output.pdf')));
 
-    // keep at the top
-    if (page === 0) {
-      const pageNum = Math.floor(i / pageSize) + 1
-      console.log('pageNum', pageNum)
-      doc.fontSize(pageNumFontsize);
-      doc.font(pageNumFont).text(`页码:${pad(pageNum, 3)}`, pageNumX, pageNumY, {
-        // width: pageNumW, height: pageNumH,
-        align: 'left'
+    // Embed a font, set the font size, and render some text
+    // doc
+    //   .font('fonts/PalatinoBold.ttf')
+    //   .fontSize(25)
+    //   .text('Some text with an embedded font!', 100, 100);
+
+    // Add an image, constrain it to a given size, and center it vertically and horizontally
+
+    images.forEach((img, i) => {
+
+      const page = i % pageSize
+
+      if (page === 0 && i > 0) {
+        console.log('new pdf page', i)
+        // Add another page
+        doc.addPage()
+      }
+
+      const row = Math.floor(page / cols)
+      const col = page % cols
+      // console.log(row, col, img)
+      var _ = doc.image(img, col * (width + marginX) + paddingX, row * (height + marginY) + paddingY, {
+        width, height,
+        // fit: [150, 200],
+        // align: 'center',
+        // valign: 'center' 
       });
-    }
-  })
 
-  // Finalize PDF file
-  doc.end();
+      // keep at the top
+      if (page === 0) {
+        const pageNum = Math.floor(i / pageSize) + 1
+        console.log('pageNum', pageNum)
+        doc.fontSize(pageNumFontsize);
+        doc.font(pageNumFont).text(`页码:${pad(pageNum, 3)}`, pageNumX, pageNumY, {
+          // width: pageNumW, height: pageNumH,
+          align: 'left'
+        });
+      }
+    })
+
+    // Finalize PDF file
+    doc.end();
+  }
+  while (images.length > 0) {
+    _func(images.splice(0, size))
+  }
+
+
 
 
   // For cutting
