e32c08644abe6eb4437f8ec1927860b252e4a8bd
Your Name <you@example.com>
2021-08-12 17:27:22 +0800

    test using bytes with cgo

--- hello.go
+++ hello.go
@@ -4,7 +4,8 @@ package main
 #cgo windows CFLAGS: -DCGO_OS_WINDOWS=1 -I .
 #cgo LDFLAGS: -L${SRCDIR} -lhello
 #include <stdio.h>
-#include <stdlib.h> //cstring 用到?
+#include <stdlib.h> //cstring 用到?'
+#include <string.h>
 #include "hello.h" //注意只能是调c的.h,不能为.hpp
 
 #if defined(CGO_OS_WINDOWS)
@@ -18,6 +19,13 @@ int GoAdd(int a, int b);
 const char* hello(char *str){
     printf("hello %s on %s\n", str, os);
     return "bar";
+}
+const void* abc(void *str){
+	char dest[50];
+	memset(dest, 0, sizeof dest);
+	memcpy(dest, str, 3);
+	printf("say %s\n", (const char*)dest);
+	return "Efg";
 }
 void printint(int v) {
     printf("printint: %d\n", GoAdd(v,v));
@@ -33,9 +41,13 @@ func hello() {
 	v := 42
 	C.printint(C.int(v))
 	foo := C.CString("foo")
+	abc := C.CBytes([]byte{'a', 'b', 'c'})
 	bar := C.hello(foo)
-	fmt.Println("hello from", C.GoString(bar))
+	efg := C.abc(abc)
+	fmt.Println("from hello:", C.GoString(bar))
+	fmt.Println("from abc:", string(C.GoBytes(efg, 3)))
 	C.hello_world()
 
 	defer C.free(unsafe.Pointer(foo))
+	defer C.free(abc)
 }
