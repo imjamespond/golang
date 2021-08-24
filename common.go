e24c7965d14b286a2c48940676e02bc147dbf71c
Your Name <you@example.com>
2021-08-24 17:06:08 +0800

    format date

--- common.go
+++ common.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func PanicIf(err error) {
@@ -35,6 +36,14 @@ func Log(data interface{}) {
 	bytes, err := json.Marshal(data)
 	ErrorIf(err)
 	log.Println(string(bytes))
+}
+
+// 需要注意的是Go语言中格式化时间模板不是常见的Y-m-d H:M:S而是使用Go语言的诞生时间 2006-01-02 15:04:05 -0700 MST。
+func Date(t *time.Time) string {
+	if t == nil {
+		return "2006-01-02,15:04:05"
+	}
+	return t.Format("2006-01-02,15:04:05")
 }
 
 func ExecCmd(command string) string {
